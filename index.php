73a6583354d5dbc4c97c01f5a6d40eea55175811
jonasanne <32570643+jonasanne@users.noreply.github.com>
2018-11-10 19:31:57 +0100

    10/11/18

--- index.php
+++ index.php
@@ -12,108 +12,79 @@
 <body>
 <header>
 
-<nav class="container">
-<!-- <h1>Website</h1> -->
-<img src="image/minifig2.png" alt="">
-<ul>
-    <li><a href="http://">Home</a></li>
-    <li><a href="http://">Lego</a></li>
-    <li><a href="http://">Lego</a></li>
-</ul>
-
+<nav class="navbar navbar-expand-lg navbar-light container">
+  <button class="navbar-toggler mr-auto" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="#"><img src="image/minifig2.png" alt="" id="minifigure"></a>
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+   
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Lego Sets</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link " href="#">Mijn pagina</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-dark my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
 </nav>
 
 
 
 </header>
 <main>
-        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-  </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="image/falcon.jpg" alt="First slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="image/aragog.jpg" alt="Second slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="image/creator.jpg" alt="Third slide">
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
+
 <div class="container" id="moc">
   <h2>Moc's</h2>
-  <hr>
+  
   <div class="row">
-    <div class="col-xs-6 col-sm-3 col-md-2 col-condensed">
-    <a href="" ><img src="image/moc's/browser.jpg" alt=""></a>
+    <div class="col-xs-6 col-sm-6 col-md-3 col-lg-2">
+    <a href=""><img src="image/moc's/browser.jpg" alt=""></a>
     </div>
-    <div class="col-xs-6 col-sm-3 col-md-2 col-condensed">
+   <div class="col-xs-6 col-sm-6 col-md-3 col-lg-2">
     <a href=""><img src="image/moc's/bus.jpg" alt=""></a>
     </div>
-    <div class="col-xs-6 col-sm-3 col-md-2 col-condensed">
+   <div class="col-xs-6 col-sm-6 col-md-3 col-lg-2">
     <a href=""><img src="image/moc's/caravan.jpg" alt=""></a>
     </div>
-     <div class="col-xs-6 col-sm-3 col-md-2 col-condensed">
+    <div class="col-xs-6 col-sm-6 col-md-3 col-lg-2">
     <a href=""><img src="image/moc's/castle.jpg" alt=""></a>
     </div>
-     <div class="col-xs-6 col-sm-3 col-md-2 col-condensed">
+    <div class="col-xs-6 col-sm-6 col-md-3 col-lg-2">
     <a href=""><img src="image/moc's/clark.jpg" alt=""></a>
     </div>
-    <div class="col-xs-6 col-sm-3 col-md-2 col-condensed">
+   <div class="col-xs-6 col-sm-6 col-md-3 col-lg-2">
     <a href=""><img src="image/moc's/groot.jpg" alt=""></a>
     </div>
-    <div class="col-xs-6 col-sm-3 col-md-2 col-condensed">
+   <div class="col-xs-6 col-sm-6 col-md-3 col-lg-2">
     <a href="" ><img src="image/moc's/browser.jpg" alt=""></a>
     </div>
-    <div class="col-xs-6 col-sm-3 col-md-2 col-condensed">
+   <div class="col-xs-6 col-sm-6 col-md-3 col-lg-2">
     <a href=""><img src="image/moc's/bus.jpg" alt=""></a>
     </div>
-    <div class="col-xs-6 col-sm-3 col-md-2 col-condensed">
+    <div class="col-xs-6 col-sm-6 col-md-3 col-lg-2">
     <a href=""><img src="image/moc's/caravan.jpg" alt=""></a>
     </div>
-    <div class="col-xs-6 col-sm-3 col-md-2 col-condensed">
+    <div class="col-xs-6 col-sm-6 col-md-3 col-lg-2">
     <a href=""><img src="image/moc's/castle.jpg" alt=""></a>
     </div >
-    <div class="col-xs-6 col-sm-3 col-md-2 col-condensed">
+    <div class="col-xs-6 col-sm-6 col-md-3 col-lg-2">
     <a href=""><img src="image/moc's/clark.jpg" alt=""></a>
     </div>
-    <div class="col-xs-6 col-sm-3 col-md-2 col-condensed">
+    <div class="col-xs-6 col-sm-6 col-md-3 col-lg-2 ">
     <a href=""><img src="image/moc's/groot.jpg" alt=""></a>
     </div>
-    <div class="col-xs-6 col-sm-3 col-md-2 col-condensed">
-    <a href="" ><img src="image/moc's/browser.jpg" alt=""></a>
-    </div>
-    <div class="col-xs-6 col-sm-3 col-md-2 col-condensed">
-    <a href=""><img src="image/moc's/bus.jpg" alt=""></a>
-    </div> 
-    <div class="col-xs-6 col-sm-3 col-md-2 col-condensed">
-    <a href=""><img src="image/moc's/caravan.jpg" alt=""></a>
-    </div>
-    <div class="col-xs-6 col-sm-3 col-md-2 col-condensed">
-    <a href=""><img src="image/moc's/castle.jpg" alt=""></a>
-    </div>
-    <div class="col-xs-6 col-sm-3 col-md-2 col-condensed">
-    <a href=""><img src="image/moc's/clark.jpg" alt=""></a>
-    </div>
-    <div class="col-xs-6 col-sm-3 col-md-2 col-condensed">
-    <a href=""><img src="image/moc's/groot.jpg" alt=""></a>  
-  </div>
   </div>
   <div class="margin-top-30 text-center">
-  <button type="button" class="btn btn-light"><a href="">Bekijk hier meer moc's!</a></button>
+  <button type="button" class="btn btn-outline-dark"><a href="">Bekijk hier alle moc's!</a></button>
   </div>
 </div>
 <div class="container">
